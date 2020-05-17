5448424fa998c27c34836579ecebb9fba6822829
Ryan Camp <campryan@comcast.net>
2020-05-17 15:14:40 -0600

    Implement item merchant api show

---
+++ app/controllers/api/v1/item_merchant_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::ItemMerchantController < ApplicationController
+  def show
+    merchant = Item.find(params[:item_id]).merchant
+    render json: MerchantSerializer.new(merchant).serialized_json
+  end
+end
