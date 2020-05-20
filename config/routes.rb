2250cdf4cf2b03cf259f58725d75ab51eab4a9ce
Ryan Camp <campryan@comcast.net>
2020-05-20 14:33:05 -0600

    Create route for most items api endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
         get '/find', to: 'search#show'
         get '/find_all', to: 'search#index'
         get '/most_revenue', to: 'most_revenue#index'
+        get '/most_items', to: 'most_items#index'
       end
       resources :merchants, only: [:index, :show, :create, :update, :destroy] do
         resources :items, only: [:index], controller: 'merchant_items'
