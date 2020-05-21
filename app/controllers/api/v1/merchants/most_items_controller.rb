1fb1e3456cea1b3edc4c061b484da493eb3015c0
Ryan Camp <campryan@comcast.net>
2020-05-20 18:07:53 -0600

    Implement merchant with most items sold

--- app/controllers/api/v1/merchants/most_items_controller.rb
+++ app/controllers/api/v1/merchants/most_items_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::Merchants::MostItemsController < ApplicationController
   def index
-
+    merchants = Merchant.joins(:transactions).
+    where(transactions: { result: 'success' }).
+    select('merchants.*, sum(invoice_items.quantity) as total_quantity').
+    group(:id).
+    order(total_quantity: :desc).
+    limit(params[:quantity])
+    render json: MerchantSerializer.new(merchants)
   end
 end
