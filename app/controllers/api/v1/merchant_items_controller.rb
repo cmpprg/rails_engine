1904daaec4e7c17ca35a7392bea3f0db407890d4
Ryan Camp <campryan@comcast.net>
2020-05-20 20:02:20 -0600

    Abstract away item serializer to private method

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::MerchantItemsController < ApplicationController
   def index
     merchant_items = Merchant.find(params[:merchant_id]).items
-    render json: ItemSerializer.new(merchant_items)
+    render json: serialize_item(merchant_items)
+  end
+
+  private
+
+  def serialize_item(items)
+    ItemSerializer.new(items)
   end
 end
