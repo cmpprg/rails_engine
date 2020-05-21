1cc417a6251c95fd8458128e935da6cec59175d8
Ryan Camp <campryan@comcast.net>
2020-05-20 19:56:07 -0600

    Create spacing for clarity

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,22 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+
       namespace :merchants do
         get '/find', to: 'search#show'
         get '/find_all', to: 'search#index'
         get '/most_revenue', to: 'most_revenue#index'
         get '/most_items', to: 'most_items#index'
       end
+
       resources :merchants, only: [:index, :show, :create, :update, :destroy] do
         resources :items, only: [:index], controller: 'merchant_items'
       end
+
       resources :items, only: [:index, :show, :create, :update, :destroy] do
         get '/merchant', to: 'item_merchant#show'
       end
+
     end
   end
 end
