41f37ac8495bc7891af9614534e49795f57f176e
Ryan Camp <campryan@comcast.net>
2020-05-20 18:08:41 -0600

    Add note to move functionality out of controller and into model

--- app/controllers/api/v1/merchants/most_items_controller.rb
+++ app/controllers/api/v1/merchants/most_items_controller.rb
@@ -1,11 +1,13 @@
 class Api::V1::Merchants::MostItemsController < ApplicationController
   def index
+    #should be in model vvvvv
     merchants = Merchant.joins(:transactions).
     where(transactions: { result: 'success' }).
     select('merchants.*, sum(invoice_items.quantity) as total_quantity').
     group(:id).
     order(total_quantity: :desc).
     limit(params[:quantity])
+    #should be in model ^^^^^
     render json: MerchantSerializer.new(merchants)
   end
 end
